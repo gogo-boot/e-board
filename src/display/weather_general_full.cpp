6b290e58a49f23fbb06a0665be4ce6899baabf9f
Jinwoo Wang <mgcos82@gmail.com>
2025-08-29 05:33:04 +0200

    wifi status. bug...

--- src/display/weather_general_full.cpp
+++ src/display/weather_general_full.cpp
@@ -150,5 +150,5 @@ void WeatherFullDisplay::drawFullScreenWeatherLayout(const WeatherInfo& weather,
 
 void WeatherFullDisplay::drawWeatherFooter(int16_t x, int16_t y, int16_t h) {
     // Use common footer with time and refresh icon
-    CommonFooter::drawFooter(x, y, h, FOOTER_TIME | FOOTER_REFRESH);
+    CommonFooter::drawFooter(x, y, h, FOOTER_TIME | FOOTER_REFRESH | FOOTER_WIFI);
 }
