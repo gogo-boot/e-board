0ad5137f2ff9357c15f86616da8cba7d4d2cdfb1
Jinwoo Wang <mgcos82@gmail.com>
2025-12-09 20:34:10 +0100

    serial monitor wait delay 3 seconds

--- src/util/system_init.cpp
+++ src/util/system_init.cpp
@@ -26,7 +26,7 @@ namespace SystemInit {
         esp_log_level_set("*", ESP_LOG_ERROR);
 #else
         Serial.begin(115200);
-        delay(5000);
+        delay(3000);
         esp_log_level_set("*", ESP_LOG_DEBUG);
 #endif
     }
